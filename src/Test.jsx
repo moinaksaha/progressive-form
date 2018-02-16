366868683944e3e6c8d30a04d9cd1ade37fc30b5
Moinak <moinak.iitr@gmail.com>
2018-02-16 12:43:20 +0530

    First commit to repo

---
+++ src/Test.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+// import styles from '../../containers/Book/Book.css';
+
+export default class Template extends Component{
+
+	constructor(){
+    	super();
+  	}
+
+	render = () => {
+		return (
+			<div>moinak test</div>
+		);
+	}
+};
+
+Template.defaultProps = {
+
+};
